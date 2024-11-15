f2a465d3eeee22913566aa7a80049c998858c320
Zainuddin Siddiqi <57043176+z-siddiqi@users.noreply.github.com>
2024-11-15 21:10:59 +0000

    add landing page with hero and newsletter signup

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,49 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen p-8 sm:p-20">
-      <main>
-        <Image
-          src="/griot.png"
-          alt="Griot logo"
-          width={180}
-          height={38}
-          priority
-        />
-      </main>
-    </div>
+    <section className="text-gray-600">
+      <div className="max-w-7xl mx-auto flex px-5 py-24 flex-col items-center md:flex-row">
+        <div className="flex flex-col pt-6 mb-20 md:mb-40 items-center text-center lg:flex-grow md:w-1/2 md:ml-24 md:items-start md:text-left">
+          <h1 className="mb-5 text-5xl sm:text-6xl items-center xl:w-2/2 text-gray-900 font-bold">
+            Connect dots, elevate your thinking
+          </h1>
+          <p className="mb-4 xl:w-3/4 text-lg text-gray-600">
+            You deserve a tool that helps you focus on the information that truly matters
+          </p>
+        </div>
+        <div className="mx-auto lg:mr-36">
+          <Image src="/phone.png" alt="phone" width={325} height={325} />
+        </div>
+      </div>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
+        <div className="py-24 md:py-36">
+          <h1 className="mb-5 text-5xl sm:text-6xl text-gray-900 font-semibold">
+            Subscribe to our newsletter
+          </h1>
+          <h1 className="mb-9 text-2xl text-gray-600 font-semibold">
+            Enter your email address to start your knowledge journey.
+          </h1>
+          <div className="mx-auto md:w-3/4">
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
+            <div className="w-full sm:w-3/4 flex mx-auto relative border border-gray-600 rounded-lg">
+              <input
+                type="email"
+                id="email"
+                className="py-3 px-4 block w-full rounded-s-lg focus:ring-0 focus:ring-offset-0"
+              />
+              <button
+                type="button"
+                className="py-3 px-4 inline-flex justify-center items-center gap-x-2 rounded-e-md bg-orange-400"
+              >
+                <span className="font-semibold text-md text-white pt-1">Subscribe</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
