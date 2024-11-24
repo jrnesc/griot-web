f225cff69ed6c21b5a3c9351ce1a98cf50fa066c
jrnesc <78368044+jrnesc@users.noreply.github.com>
2024-11-24 17:58:33 +0000

    centering footer elements when screen size is small

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="pb-4">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 md:px-8">
         <div className="flex flex-col-reverse justify-between pt-5 pb-4 bg-top border-black lg:flex-row">
-          <ul className="flex flex-col space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
+          <ul className="flex flex-col space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row sm:justify-center items-center">
             <li>
               <Link href="/privacy-policy" className="text-md text-black font-semibold">
                 Privacy Policy
@@ -22,8 +22,8 @@ export default function Footer() {
               </Link>
             </li> */}
           </ul>
-          <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
-            <Link href="/" className="text-md text-orange-400 font-semibold tracking-tight">
+          <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row sm:justify-center items-center">
+            <Link href="/" className="text-md text-black font-semibold">
               © 2024 Griot
             </Link>
           </ul>
