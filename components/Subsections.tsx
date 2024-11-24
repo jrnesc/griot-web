c4122b2442b01520a4b88846b4448bb0ee651a23
jrnesc <78368044+jrnesc@users.noreply.github.com>
2024-11-24 17:52:06 +0000

    adding (placeholder) header options, centering desc text

--- components/Subsections.tsx
+++ components/Subsections.tsx
@@ -10,7 +10,7 @@ export default function Subsections() {
         <div className="p-6 w-full md:w-1/2">
           <img src="/social+comp.png" alt="Phone" className="w-full max-w-md mx-auto" />
         </div>
-        <div className="ml-0 md:ml-8 w-full md:w-1/2 text-left">
+        <div className="ml-0 md:ml-8 w-full md:w-1/2 text-left flex items-center px-8 py-4">
           <p className="text-lg text-gray-600">Upload PDFs, ePUBs, YouTube videos, Google Docs and Obsidian notes, and Griot will create a search engine so you can start making connections no-one has seen before. Powered by GPT4, Gemini and Claude.</p>
         </div>
       </div>
@@ -24,7 +24,7 @@ export default function Subsections() {
         <div className="p-6 w-full md:w-1/2">
           <img src="/search+gradient.png" alt="Phone" className="w-full max-w-md mx-auto" />
         </div>
-        <div className="ml-0 md:ml-8 w-full md:w-1/2 text-left">
+        <div className="ml-0 md:ml-8 w-full md:w-1/2 text-left flex items-center px-8 py-4">
           <p className="text-lg text-gray-600">Having a curated space for your own sources reduces your time to mental clarity. Griot becomes an expert on the sources that you draw inspiration from.</p>
         </div>
       </div>
@@ -39,7 +39,7 @@ export default function Subsections() {
         <div className="p-6 w-full md:w-1/2">
           <img src="/search+gradient.png" alt="Phone" className="w-full max-w-md mx-auto" />
         </div>
-        <div className="ml-0 md:ml-8 w-full md:w-1/2 text-left">
+        <div className="ml-0 md:ml-8 w-full md:w-1/2 text-left flex items-center px-8 py-4">
           <p className="text-lg text-gray-600">Griot provides quotes and references from your sources with every answer. Never second guess, validate and expand your own understanding.</p>
         </div>
       </div>
