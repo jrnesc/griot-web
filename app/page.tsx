4b9276cac06eb6e540a1c7cfe1487b36f4a03a47
jrnesc <78368044+jrnesc@users.noreply.github.com>
2024-11-24 18:06:06 +0000

    separating field and button

--- app/page.tsx
+++ app/page.tsx
@@ -30,23 +30,20 @@ export default function Home() {
           <h1 className="mb-9 text-xl sm:text-2xl text-gray-600 font-semibold">
             Join our newsletter to improve your learning journey
           </h1>
-          <div className="mx-auto md:w-1/2">
+          <div className="mx-auto md:w-1/2 flex items-center">
             <label htmlFor="email" className="sr-only">
               Email
             </label>
-            <div className="w-full sm:w-3/4 flex mx-auto relative border border-gray-600 rounded-lg">
+            <div className="w-full sm:w-3/4 flex relative border border-gray-600 rounded-2xl mr-2">
               <input
                 type="email"
                 id="email"
-                className="py-3 px-4 block w-full rounded-s-lg focus:ring-0 focus:ring-offset-0"
+                className="py-3 px-4 block w-full rounded-2xl focus:ring-0 focus:ring-offset-0"
               />
-              <button
-                type="button"
-                className="py-3 px-4 inline-flex justify-center items-center gap-x-2 rounded-e-md bg-orange-400"
-              >
-                <span className="font-semibold text-md text-white pt-1">Subscribe</span>
-              </button>
             </div>
+            <button type="button" className="py-3 px-4 inline-flex justify-center items-center gap-x-2 rounded-2xl bg-orange-400">
+                <span className="font-semibold text-md text-white pt-1">Subscribe</span>
+            </button>
           </div>
         </div>
       </div>
