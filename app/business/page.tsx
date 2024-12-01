76376c8154c1ee9e7fa1e8da5ff5055ae4968eca
jrnesc <78368044+jrnesc@users.noreply.github.com>
2024-12-01 02:53:41 +0000

    initial b2b signup page

--- app/business/page.tsx
+++ app/business/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
 import Image from 'next/image';
-import Subsections from '@/components/Subsections';
-import { createContact } from '@/app/api/send/route';
+import React from 'react';
 import { useState } from 'react';
+import { createContact } from '@/app/api/send/route';
 import SubscribeButton from '@/components/SubscribeButton';
 
-export default function Home() {
+export default function BusinessPage() {
   const [email, setEmail] = useState('');
 
   const handleSubscribe = async (email: string) => {
@@ -18,30 +18,30 @@ export default function Home() {
       console.error('Subscription failed:', error);
     }
   };
-
   return (
     <section className="text-gray-600">
       <div className="max-w-7xl mx-auto flex flex-col px-5 py-36 items-center md:flex-row">
-        <div className="flex flex-col pt-6 mb-20 md:mb-40 items-center text-center lg:flex-grow md:w-1/2 md:ml-24 md:items-start md:text-left">
+        <div className="flex flex-col items-center pt-6 mb-20 md:mb-40 text-center lg:flex-grow md:w-1/2 md:ml-24">
+          <h2 className="mb-4 xl:w-3/4 text-lg text-600 font-bold">
+            Coming soon
+          </h2>
           <h1 className="mb-5 text-4xl sm:text-5xl items-center xl:w-2/2 text-gray-900 font-bold">
-            Personal AI Search
+            Griot for businesses
           </h1>
-          <p className="mb-4 xl:w-3/4 text-lg text-gray-600">
-            Create a space from your most important sources
+          <p className="mb-16 xl:w-3/4 text-lg text-gray-600">
+            Integrate Confluence, JIRA, GitHub and MongoDB into a single hub.<br/><br/>Save money and time planning and building.
           </p>
-        </div>
-        <div className="mx-auto lg:mr-36 md:ml-4">
-          <Image src="/app.png" alt="phone" width={250} height={100} className="w-full max-w-xs" />
+          <div className="flex justify-center w-full max-w-full" style={{ height: 'auto' }}>
+            <Image src="/business.png" alt="phone" width={500} height={300} className="w-full h-auto" />
+          </div>
         </div>
       </div>
 
-      <div className="mt-0 mb-0"><Subsections /></div>
-
       <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
         <div className="py-24 md:py-36">
-          <h1 className="mb-5 text-4xl sm:text-5xl text-gray-900 font-semibold">Subscribe</h1>
-          <h1 className="mb-9 text-xl sm:text-2xl text-gray-600 font-semibold">Join our newsletter to improve your learning journey</h1>
-          <div className="mx-auto md:w-1/2 flex items-center">
+          <h1 className="mb-5 text-4xl sm:text-5xl text-gray-900 font-semibold">Start streamlining product development</h1>
+          <h1 className="mb-9 text-xl sm:text-2xl text-gray-600 font-semibold">Sign up to to book your demo</h1>
+          <div className="mx-auto md:w-1/2 flex flex-col md:flex-row items-center">
             <label htmlFor="email" className="sr-only">Email</label>
             <div className="w-full sm:w-3/4 flex relative border border-gray-600 rounded-2xl mr-2">
               <input
@@ -51,11 +51,12 @@ export default function Home() {
                 onChange={(e) => setEmail(e.target.value)} // Capture email input
               />
             </div>
-
-            <SubscribeButton email={email} onSubscribe={handleSubscribe} />
+            <div className="mt-2 md:mt-0">
+              <SubscribeButton email={email} onSubscribe={handleSubscribe} />
+            </div>
           </div>
         </div>
       </div>
-    </section>
+    </ section>
   );
 }
