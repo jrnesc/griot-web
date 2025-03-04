c635203ab1a0abd5f6e5458d54c4fc6361303114
jrnesc <78368044+jrnesc@users.noreply.github.com>
2025-03-04 13:22:16 +0000

    typo

--- components/Subsections.tsx
+++ components/Subsections.tsx
@@ -30,7 +30,7 @@ export default function Subsections() {
         </div>
         <div className="ml-0 md:ml-8 w-full md:w-1/2 text-left flex items-center px-8 py-4">
           <p className="text-lg text-gray-600">
-            Create your own articles + mini-courses from any of your sources. You can compare ideas from your favourite authors, analyse your notes or simplyconsume your content in an easier medium
+            Create your own articles + mini-courses from any of your sources. You can compare ideas from your favourite authors, analyse your notes or simply consume your content in an easier medium
           </p>
         </div>
       </div>
